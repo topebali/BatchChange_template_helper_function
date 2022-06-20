8ff8c77fc4a69d7f849b3cc4a1b0678aa9f2435b
topebali <38654489+topebali@users.noreply.github.com>
2022-06-20 15:42:42 +0100

    Create file1.rb

---
+++ file1.rb
@@ -0,0 +1,7 @@
+add_method_tracer(:foo, :bar) 
+# pre-sort to normalize default array across platforms and then proceed to create a Hash
+# from that sorted array.
+# tttt
+add_method_tracer :foo, :bar 
+add_method_tracer :foo,
+                  :bar // that's a multi-line method call without parentheses
